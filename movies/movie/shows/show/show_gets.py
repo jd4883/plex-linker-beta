7891d60486e19d85566106e914f02881e6a76cb6
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-08-26 13:00:43 -0700

    methods cleanup testing

--- movies/movie/shows/show/show_gets.py
+++ movies/movie/shows/show/show_gets.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-from messaging.frontend import (method_exit,
-                                method_launch,
-                                print_method_shows_dictionary_value,
-                                print_shows_dictionary)
+from messaging.frontend import (
+	method_exit,
+	method_launch,
+	print_method_shows_dictionary_value,
+	print_shows_dictionary,
+	)
 from movies.movie.shows.show.show_puts import set_show
 from movies.movie.shows.show.show_validation import (validate_show_path_presence)
 
 
-# def get_show_root_path(show_object):
-# 	method_launch(show_object)
-# 	method_exit(show_object)
-# 	if validate_show_path_presence(show_object):
-# 		return True
-# 	return False
+def get_show_root_path(show_object):
+	method_launch(show_object)
+	method_exit(show_object)
+	if validate_show_path_presence(show_object):
+		return True
+	return False
 
 
 def get_show(show_object):
@@ -37,13 +39,17 @@ def create_tv_show_class_object(movie_object,
 	method_exit(movie_object)
 	return tv_show_class_object
 
+
 def get_fully_parsed_show_with_absolute_episode(show_object):
-	return f"{show_object.path}/{show_object.season_folder}/{show_object.show} - S{show_object.season}E{show_object.episode} (E{show_object.absolute_episode}) - {show_object.title}"
+	return f"{show_object.path}/{show_object.season_folder}/{show_object.show} - S{show_object.season}E{
+	show_object.episode} (E{show_object.absolute_episode}) - {show_object.title}"
 
 
 def get_fully_parsed_show_without_absolute_episode(show_object):
-	return f"{show_object.path}/{show_object.season_folder}/{show_object.show} - S{show_object.season}E{show_object.episode} - {show_object.title}"
+	return f"{show_object.path}/{show_object.season_folder}/{show_object.show} - S{show_object.season}E{
+	show_object.episode
+	}} - {show_object.title}"
 
 
 def get_alphabetical_specials_string():
-	return "Season 00" # create sonarr API call here to get the name dynamically
+	return "Season 00"  # create sonarr API call here to get the name dynamically
