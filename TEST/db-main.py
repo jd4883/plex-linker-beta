cc92d2da680ad67557ab960e0ba4b0821f48c7a8
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-08-26 12:35:50 -0700

    db test

--- TEST/db-main.py
+++ TEST/db-main.py
@@ -1,14 +1,2 @@
 #!/usr/bin/env python3.7
 
-import sqlite3
-
-
-
-def add_to_db(command):
-
-connection = sqlite3.connect("movies_collection.db")
-cursor = connection.cursor()
-cursor.execute(command)
-connection.commit()
-
-connection.close()
