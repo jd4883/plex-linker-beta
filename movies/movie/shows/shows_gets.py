7ef1f5a5df5d7d6c6e7d590257ed6087a16e6bcc
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-08-26 13:05:02 -0700

    methods cleanup testing

--- movies/movie/shows/shows_gets.py
+++ movies/movie/shows/shows_gets.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-# def get_absolute_episode_status_value_from_movies_dictionary(movies_dictionary_object,
-#                                                              show):
-# 	try:
-# 		if not movies_dictionary_object['Shows'][show]['Absolute Episode']:
-# 			movies_dictionary_object['Shows'][show]['Absolute Episode'] = False
-# 		return movies_dictionary_object['Shows'][show]['Absolute Episode']
-# 	except TypeError:
-# 		return False
-
-
-def get_shows_directories_from_yaml(inventoried_movies_from_yaml_file):
-	return inventoried_movies_from_yaml_file["Show Directories"]
+#
+#
+# def get_shows_directories_from_yaml(inventoried_movies_from_yaml_file):
+# 	return inventoried_movies_from_yaml_file["Show Directories"]
