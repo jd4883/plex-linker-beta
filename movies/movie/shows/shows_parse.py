22250b1b26c453bdad26e4b24c5ec060b1b90c73
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-03 13:07:50 -0700

    current testing version

--- movies/movie/shows/shows_parse.py
+++ movies/movie/shows/shows_parse.py
@@ -50,6 +50,7 @@ def parse_shows_dictionary_object(movie_class_object,
 		g.movies_dictionary_object[movie_class_object.movie_title]['Shows'][show]['Absolute Show Path'] = str()
 		g.movies_dictionary_object[movie_class_object.movie_title]['Shows'][show]['Parsed Relative Show Title'] = str()
 		g.movies_dictionary_object[movie_class_object.movie_title]['Shows'][show]['Show Dictionary Object'] = dict()
+		g.movies_dictionary_object[movie_class_object.movie_title]['Shows'][show]['Symlinked'] = str()
 		# make try except more specific
 		#try:
 			# this component sort of worked 9-2 but never got fully going. Trying to come up with a reasonably intelligent
