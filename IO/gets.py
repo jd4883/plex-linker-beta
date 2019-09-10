f5de7e7734071792c6bd844f76b6d03ddaafb44a
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-10 13:12:03 -0700

    current testing version

--- IO/gets.py
+++ IO/gets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def get_collection_absolute_path_parsed_this_run():
-	return str(f'/var/data/scripts/symlink_scripts/movie_tv_pairing/config_files/media_collection_parsed_this_run.yaml')
+	return str(f'/plex_linker/config_files/media_collection_parsed_this_run.yaml')
 
 
 def get_collection_absolute_path_parsed_last_run():
-	return str(f'/var/data/scripts/symlink_scripts/movie_tv_pairing/config_files/media_collection_parsed_last_run.yaml')
+	return str(f'/plex_linker/config_files/media_collection_parsed_last_run.yaml')
