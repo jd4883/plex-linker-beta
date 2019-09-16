2bcfccd21686951564b38c89c00c049f0f7c33fe
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-16 13:07:43 -0700

    current testing

--- movies/movie/shows/show/create_class_object.py
+++ movies/movie/shows/show/create_class_object.py
@@ -19,11 +19,11 @@ def create_tv_show_class_object(self,
 	# fields from here are all valid from the API call to parse out
 	
 	# need to play with radarr as well as plex API calls for more functionality and cleaning up functions
-	try:
-		print(f"Theoretically this is the show via API call: {tv_show_class_object.raw_episodes}")
-		# print(f"Theoretically this is the show via API call: {tv_show_class_object.raw_episodes[show]['episodeNumber']}")
-	except KeyError as err:
-		print(f'except hit sorry bro not the correct API call syntax, error {err}')
+	# try:
+	# 	print(f"Theoretically this is the show via API call: {tv_show_class_object.raw_episodes}")
+	# 	# print(f"Theoretically this is the show via API call: {tv_show_class_object.raw_episodes[show]['episodeNumber']}")
+	# except KeyError as err:
+	# 	print(f'except hit sorry bro not the correct API call syntax, error {err}')
 	# tv_show_class_object.raw_episodes[show]['seasonNumber']
 	# tv_show_class_object.raw_episodes[show]['episodeNumber']
 	# tv_show_class_object.raw_episodes[show]['title']
