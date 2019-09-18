99559f0a7782074d7408c2b332e28c7e7e9e62c7
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-18 12:45:23 -0700

    revisions

--- movies/movie/shows/show/show_puts.py
+++ movies/movie/shows/show/show_puts.py
@@ -54,8 +54,7 @@ def set_season_dictionary_value(show,
 	try:
 		show['Season'] = str(0)
 	except TypeError:
-		show = dict(show)
-		show['Season'] = str(0)
+		return str(0)
 	if sonarr_api_query['seasons'][0]['seasonNumber'] == 0:
 		try:
 			result = sonarr_api_query['seasons'][0].pop('seasonNumber')
