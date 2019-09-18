8f318e5682c78d1672f660ae96ac25b23253f5cc
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-18 13:15:32 -0700

    revisions

--- movies/movie/shows/show/create_class_object.py
+++ movies/movie/shows/show/create_class_object.py
@@ -13,11 +13,11 @@ def create_tv_show_class_object(self,
 	tv_show_class_object.show = show
 	get_show(tv_show_class_object,
 	         g)
-	try:
-		tv_show_class_object.raw_episodes = g.sonarr.get_episodes_by_series_id(g.movies_dictionary_object[self.movie_title]['Shows'][tv_show_class_object.show]['Show ID'])
-		tv_show_class_object.raw_episode_files = g.sonarr.get_episode_files_by_series_id(g.movies_dictionary_object[self.movie_title]['Shows'][tv_show_class_object.show]['Show ID'])
-	except KeyError as err:
-		print(f"Error grabbing raw episode data from sonarr due to {err}")
+	# try:
+	# 	tv_show_class_object.raw_episodes = g.sonarr.get_episodes_by_series_id(g.movies_dictionary_object[self.movie_title]['Shows'][tv_show_class_object.show]['Show ID'])
+	# 	tv_show_class_object.raw_episode_files = g.sonarr.get_episode_files_by_series_id(g.movies_dictionary_object[self.movie_title]['Shows'][tv_show_class_object.show]['Show ID'])
+	# except KeyError as err:
+	# 	print(f"Error grabbing raw episode data from sonarr due to {err}")
 	method_exit(g)
 	return tv_show_class_object
 
