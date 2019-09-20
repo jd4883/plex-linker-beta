15874104f8ddb16a14da5681d75defe8c788a3b1
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-20 18:52:57 -0400

    revised and working

--- movies/movie/movie_validation.py
+++ movies/movie/movie_validation.py
@@ -23,7 +23,8 @@ def validate_movie_extension(movie, g):
 				if file.endswith(file_extension):
 					set_movie_file_and_extension(file, file_extension, movie, g)
 					return True
-	return False
+	except KeyError:
+		return False
 
 
 # noinspection PyDeepBugsBinOperand
