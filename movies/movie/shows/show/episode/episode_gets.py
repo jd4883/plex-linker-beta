6956711437832e0e6f4b1191eed26ab7fa56cb76
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-19 22:23:14 -0700

    revised

--- movies/movie/shows/show/episode/episode_gets.py
+++ movies/movie/shows/show/episode/episode_gets.py
@@ -1,63 +1,48 @@
 #!/usr/bin/env python3
-from messaging.frontend import (method_launch,
-                                method_exit)
+import messaging.frontend as message
+from movies.movie.shows.show.show_gets import get_alphabetical_specials_string
 
 
-def get_season(self,
-               g):
-	method_launch(g)
+def get_season(self, g):
+	message.method_launch(g)
 	from movies.movie.shows.show.show_gets import (get_alphabetical_specials_string)
-	if g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season']:
-		g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Parsed Season'] = \
-			get_season_value_from_movies_dictionary(self.movie_dictionary_object,
-		                                                             self.show,
-		                                                             g)
-	# noinspection PyDeepBugsBinOperand
-	if (g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season'] == int(0)) or \
-			str(g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season']).isdigit():
-		g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Parsed Season'] = \
-			str(get_padded_episode_number(g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season'],
-			                              2))
+	if self.show_dictionary['Season']:
+		self.show_dictionary['Parsed Season'] =\
+			get_season_value_from_movies_dictionary(self.movie_dictionary_object, self.show, g)
+	if (self.show_dictionary['Season'] == int(0)) or str(self.show_dictionary['Season']).isdigit():
+		self.show_dictionary['Parsed Season'] = str(get_padded_episode_number(self.show_dictionary['Season'], 2))
 	else:
-		g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Parsed Season'] = \
-			str(get_alphabetical_specials_string())  # play with this
-	# add more logic to prevent non-defined dictionary_for_shows to go through
-	method_exit(g)
-	return g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Parsed Season']
+		self.show_dictionary['Parsed Season'] =\
+			str(get_alphabetical_specials_string())  # try to get to the class object level
+	message.method_exit(g)
+	return self.show_dictionary['Parsed Season']
 
 
-def get_season_folder(self,
-                      g):
-	method_launch(g)
-	from movies.movie.shows.show.show_gets import get_alphabetical_specials_string
-	if g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season'] is 0 or get_alphabetical_specials_string():
+def get_season_folder(self, g):
+	message.method_launch(g)
+	if self.show_dictionary['Season'] is 0 or get_alphabetical_specials_string():
 		self.season_folder = get_alphabetical_specials_string()
-	elif g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Season']:  # add error handling for isdigit
-		self.season_folder = \
-			f"Season {get_season(self, g)}"
+	elif self.show_dictionary['Season']:
+		self.season_folder = f"Season {get_season(self, g)}"
 	else:
-		method_exit(g)
+		message.method_exit(g)
 		return
-	method_exit(g)
-	g.movies_dictionary_object[self.movie_title]['Shows'][self.show]['Parsed Season Folder'] = \
-		self.season_folder
+	message.method_exit(g)
+	self.show_dictionary['Parsed Season Folder'] = self.season_folder
 	return self.season_folder
 
 
-def get_season_value_from_movies_dictionary(movie_dictionary_object,
-                                            show,
-                                            g):
-	method_launch(g)
+def get_season_value_from_movies_dictionary(movie, show, g):
+	message.method_launch(g)
 	try:
-		if not movie_dictionary_object['Shows'][show]['Season']:
-			movie_dictionary_object['Shows'][show]['Season'] = str()
+		if not movie['Shows'][show]['Season']:
+			movie['Shows'][show]['Season'] = str()
 	except KeyError as err:
 		print(f"{g.method} had a KeyError{err}")  # testing
-		movie_dictionary_object['Shows'][show]['Season'] = str()
-	method_exit(g)
-	return movie_dictionary_object['Shows'][show]['Season']
+		movie['Shows'][show]['Season'] = str()
+	message.method_exit(g)
+	return movie['Shows'][show]['Season']
 
 
-def get_padded_episode_number(e,
-                              num):
+def get_padded_episode_number(e, num):
 	return str(e).zfill(num)
