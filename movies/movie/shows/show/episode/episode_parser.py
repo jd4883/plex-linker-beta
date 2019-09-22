a648616c9fd8322283b323f1b86b411c5d037394
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-21 22:38:58 -0400

    revisd

--- movies/movie/shows/show/episode/episode_parser.py
+++ movies/movie/shows/show/episode/episode_parser.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 def parse_season_using_sonarr_api(show, query):
 	show['Season'] = int(0) if not show['Season'] else show['Season']
-	if query['seasons']:
-		for item in query['seasons']:
-			if item == {'monitored': True} or show[item]['seasonNumber'] == 0:
-				show['Season'] = int(0)
-				break
-	else:
-		show['Parsed Season'] = str(show['Season']).zfill(2)
+	try:
+		if query['seasons']:
+			for item in query['seasons']:
+				if item == {'monitored': True} or show[item]['seasonNumber'] == 0:
+					show['Season'] = int(0)
+					break
+	except AttributeError:
+		pass
+	show['Parsed Season'] = str(show['Season']).zfill(2)
