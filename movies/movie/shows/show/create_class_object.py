27f405f1fc5d424f55c9036e9cb4ed55a8113a73
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-09-27 16:34:06 -0700

    lots of small tweaks and cleanup

--- movies/movie/shows/show/create_class_object.py
+++ movies/movie/shows/show/create_class_object.py
@@ -10,11 +10,11 @@ def init_show_object(movie,
 	except AttributeError:
 		return
 	get_show(show, g)
-	# try:
-	# 	tv_show_class_object.raw_episodes = g.sonarr.get_episodes_by_series_id(g.movies_dictionary_object[movie.movie_title]['Shows'][tv_show_class_object.movie]['Show ID'])
-	# 	tv_show_class_object.raw_episode_files = g.sonarr.get_episode_files_by_series_id(g.movies_dictionary_object[movie.movie_title]['Shows'][tv_show_class_object.movie]['Show ID'])
-	# except KeyError as err:
-	# 	print(f"Error grabbing raw episode data from sonarr due to {err}")
+	try:
+		show.raw_episodes = g.sonarr.get_episodes_by_series_id(show.show_dictionary['Show ID'])
+		show.raw_episode_files = g.sonarr.get_episode_files_by_series_id(show.show_dictionary['Show ID'])
+	except KeyError as err:
+		print(f"Error grabbing raw episode data from sonarr due to {err}")
 	return show
 
 
