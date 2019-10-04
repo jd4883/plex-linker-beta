d3856cb70a9a9a5e803cdb131f12c1ef62ac9f15
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-04 13:30:33 -0700

    added verbosity to see what is not processing

--- jobs/cleanup/cleanup.py
+++ jobs/cleanup/cleanup.py
@@ -26,35 +26,35 @@ def cleanup_sonarr_api_query(result):
 	# need to make this a dynamic alternative to the ugly code here
 	i = 0
 	while i < len(result):
-		j = 0
-		while j < len(cleanup_list):
-			if result[i][j] in cleanup_list:
-				result[i].pop(j)
-			j += 1
-		print('completed i j loop')
-		# try:
-		# 	result[i].pop('overview')
-		# 	result[i].pop('status')
-		# 	result[i].pop('network')
-		# 	result[i].pop('cleanTitle')
-		# 	result[i].pop('useSceneNumbering')
-		# 	result[i].pop('runtime')
-		# 	result[i].pop('images')
-		# 	result[i].pop('seasonCount')
-		# 	result[i].pop('added')
-		# 	result[i].pop('sortTitle')
-		# 	result[i].pop('titleSlug')
-		# 	result[i].pop('profileId')
-		# 	result[i].pop('tags')
-		# 	result[i].pop('ratings')
-		# 	result[i].pop('qualityProfileId')
-		# 	result[i].pop('tvRageId')
-		# 	result[i].pop('tvMazeId')
-		# 	result[i].pop('firstAired')
-		# 	result[i].pop('languageProfileId')
-		# 	result[i].pop('seasonFolder')
-		# except KeyError as err:
-		# 	print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
+		# j = 0
+		# while j < len(cleanup_list):
+		# 	if result[i][j] in cleanup_list:
+		# 		result[i].pop(j)
+		# 	j += 1
+		# print('completed i j loop')
+		try:
+			result[i].pop('overview')
+			result[i].pop('status')
+			result[i].pop('network')
+			result[i].pop('cleanTitle')
+			result[i].pop('useSceneNumbering')
+			result[i].pop('runtime')
+			result[i].pop('images')
+			result[i].pop('seasonCount')
+			result[i].pop('added')
+			result[i].pop('sortTitle')
+			result[i].pop('titleSlug')
+			result[i].pop('profileId')
+			result[i].pop('tags')
+			result[i].pop('ratings')
+			result[i].pop('qualityProfileId')
+			result[i].pop('tvRageId')
+			result[i].pop('tvMazeId')
+			result[i].pop('firstAired')
+			result[i].pop('languageProfileId')
+			result[i].pop('seasonFolder')
+		except KeyError as err:
+			print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
 		i += 1
 	return result
 
