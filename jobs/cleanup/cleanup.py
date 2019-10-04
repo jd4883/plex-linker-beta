9c5cbee7c3e36c1b374684f7909bb688b90772f5
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-04 13:12:39 -0700

    added verbosity to see what is not processing

--- jobs/cleanup/cleanup.py
+++ jobs/cleanup/cleanup.py
@@ -30,38 +30,37 @@ def cleanup_sonarr_api_query(result):
 		# while j < len(cleanup_list):
 		# 	result[i].pop(j)
 		# 	j += 1
-		result[i].pop('overview')
-		result[i].pop('status')
-		result[i].pop('network')
-		result[i].pop('cleanTitle')
-		result[i].pop('useSceneNumbering')
-		result[i].pop('runtime')
-		result[i].pop('images')
-		result[i].pop('seasonCount')
-		result[i].pop('added')
-		result[i].pop('sortTitle')
-		result[i].pop('titleSlug')
-		result[i].pop('profileId')
-		result[i].pop('tags')
-		result[i].pop('ratings')
-		result[i].pop('qualityProfileId')
-		result[i].pop('tvRageId')
-		result[i].pop('tvMazeId')
-		result[i].pop('firstAired')
-		result[i].pop('languageProfileId')
-		result[i].pop('seasonFolder')
 		try:
-			result[i].pop('lastInfoSync')
+			result[i].pop('overview')
+			result[i].pop('status')
+			result[i].pop('network')
+			result[i].pop('cleanTitle')
+			result[i].pop('useSceneNumbering')
+			result[i].pop('runtime')
+			result[i].pop('images')
+			result[i].pop('seasonCount')
+			result[i].pop('added')
+			result[i].pop('sortTitle')
+			result[i].pop('titleSlug')
+			result[i].pop('profileId')
+			result[i].pop('tags')
+			result[i].pop('ratings')
+			result[i].pop('qualityProfileId')
+			result[i].pop('tvRageId')
+			result[i].pop('tvMazeId')
+			result[i].pop('firstAired')
+			result[i].pop('languageProfileId')
+			result[i].pop('seasonFolder')
 		except KeyError as err:
 			print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
-		try:
-			result[i].pop('certification')
-		except KeyError as err:
-			print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
-		try:
-			result[i].pop('remotePoster')
-		except KeyError as err:
-			print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
+		# try:
+		# 	result[i].pop('certification')
+		# except KeyError as err:
+		# 	print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
+		# try:
+		# 	result[i].pop('remotePoster')
+		# except KeyError as err:
+		# 	print(f"KEY ERROR POPPING UNWANTED FIELDS {err}")
 		i += 1
 	return result
 
