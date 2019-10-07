a2389c690b217e72004503d6801029ec4c35ad1d
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-07 12:55:22 -0700

    testing prints

--- movies/movie/movie_parser.py
+++ movies/movie/movie_parser.py
@@ -4,7 +4,7 @@ import messaging.frontend as message
 def extension_from_movie_file(movie,
                               g):
 	message.method_launch(g)
-	movie.quality = parse_proper(movie)
+	movie.quality = parse_proper(movie, g)
 	message.print_movie_file_found_to_parse(movie, g)
 	message.print_movie_extension_found_to_parse(movie, g)
 	message.print_movie_file_quality(movie, g)
@@ -23,7 +23,7 @@ def parse_movie_file_quality(movie_file):
 	return movie_file.split().pop()
 
 
-def parse_proper(movie):
+def parse_proper(movie, g):
 	movie.quality = parse_remux_in_quality(parse_movie_file_quality(movie.movie_file), g)
 	if movie.quality.endswith(f"Proper.{movie.extension}"):
 		movie.quality = f"{movie.movie_file.split().pop(-2)} {movie.quality}"
