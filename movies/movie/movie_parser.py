c9d0cdc02bd10ca3ff869754e34256e475c296b3
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-07 13:20:53 -0700

    testing prints

--- movies/movie/movie_parser.py
+++ movies/movie/movie_parser.py
@@ -13,13 +13,13 @@ def extension_from_movie_file(movie,
 
 # def parse_movie_file_quality(movie_file):
 # 	return movie_file.split().pop()
-
-
-def parse_quality(movie):
-	if str(movie.quality).lower() == "Remux-1080p.mkv".lower():
-		quality.replace("Remux-1080p.mkv", "Bluray-1080p Remux.mkv")
-	if quality.endswith(f"Proper.{movie.extension}"):
-		quality = f"{movie.movie_file.split().pop(-2)} {movie.quality}"
-	else:
-		quality = movie_file.split().pop()
-	return quality
+#
+#
+# def parse_quality(movie):
+# 	if str(movie.quality).lower() == "Remux-1080p.mkv".lower():
+# 		quality.replace("Remux-1080p.mkv", "Bluray-1080p Remux.mkv")
+# 	if quality.endswith(f"Proper.{movie.extension}"):
+# 		quality = f"{movie.movie_file.split().pop(-2)} {movie.quality}"
+# 	else:
+# 		quality = movie_file.split().pop()
+# 	return quality
