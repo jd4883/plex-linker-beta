0cf442b346f56dc993193a15b358d5c49a495f3b
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-14 14:44:06 -0700

    prototype link revisions

--- class_objects/__init__.py
+++ class_objects/__init__.py
@@ -222,14 +222,9 @@ class Show(Movie, Globals):
 		self.episode_file_dict = parse_series.parse_episode_file_id_dict(self, g)
 		link = str(self.episode_file_dict['path']).replace(str(os.environ['SONARR_ROOT_PATH_PREFIX']), str())
 		link = str(os.readlink(link)).replace('../', str())
-		if str(self.relative_movie_file_path) == link:
-			print("we have a match")
-		else:
-			print("no match")
-		print(f"LINK REL PATH CALCULATION FROM SYMLINK: {link}")
-		print(f"LINK REL PATH FROM RADARR: {self.relative_movie_file_path}")
-		self.has_link = self.series_dict['Show Link Status'] = bool(os.path.islink(link))
+		self.has_link = self.series_dict['Show Link Status'] = bool(os.path.islink(link)) if str(self.relative_movie_file_path) == link else bool()
 		self.link_status = fetch_series.symlink_status(self, g)
+		print(self.has_link)
 		breakpoint()
 		# need to add monitored and file status info for episodes to determine this part
 		# self.has_link =
