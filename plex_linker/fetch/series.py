0673a5aa3c7591e2f5768868e0c83e7f14ba338d
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-14 15:24:08 -0700

    prototype link revisions

--- plex_linker/fetch/series.py
+++ plex_linker/fetch/series.py
@@ -20,7 +20,7 @@ def show_path_string(string):
 def fetch_link_status(self, episode_file_dict, relative_movie_file_path):
 	print(episode_file_dict)
 	result = bool()
-	link = str(episode_file_dict['path']).replace(os.environ['SONARR_ROOT_PATH_PREFIX'],
+	link = str(episode_file_dict.pop('path')).replace(os.environ['SONARR_ROOT_PATH_PREFIX'],
 	                                                                          str())
 	parsed_link = str(os.readlink(link)).replace('../', str())
 	if str(relative_movie_file_path) == parsed_link:
