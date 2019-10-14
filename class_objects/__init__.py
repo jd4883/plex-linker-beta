f46f71c7ce4629928b5712aa29b3a61574ee21eb
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-14 13:16:59 -0700

    testing for new link checking mechanism

--- class_objects/__init__.py
+++ class_objects/__init__.py
@@ -220,9 +220,9 @@ class Show(Movie, Globals):
 		self.padding = parse_series.episode_padding(self, g)
 		self.episode_file_id = parse_series.episode_file_id(self, g)
 		self.episode_file_dict = parse_series.parse_episode_file_id_dict(self, g)
-		link_status = str(self.episode_file_dict['path']).replace(str(os.environ['SONARR_ROOT_PATH_PREFIX']))
+		link = str(self.episode_file_dict['path']).replace(str(os.environ['SONARR_ROOT_PATH_PREFIX']))
 		self.has_link = \
-			self.series_dict['Show Link Status'] = bool(True) if str(os.path.exists()) else bool()
+			self.series_dict['Show Link Status'] = bool(True) if str(os.path.exists(link)) else bool()
 		self.link_status = fetch_series.symlink_status(self, g)
 		print(self.has_link)
 		breakpoint()
