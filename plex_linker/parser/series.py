7ad072110b5afb248aad45bf85c4109ea0977000
Jacob Dresdale <dresdale.jacob@gmail.com>
2019-10-14 16:34:02 -0700

    prototype link revisions

--- plex_linker/parser/series.py
+++ plex_linker/parser/series.py
@@ -237,8 +237,11 @@ def parsed_show_title(self, g):
 
 
 def episode_title(self, g):
+	title = str()
 	for k,v in self.episode_dict.items():
-		print(k, v)
+		if k == 'title':
+			print(v)
+			breakpoint()
 	result = self.series_dict['Title'] = fetch_series.show_path_string(self.episode_dict.pop('title', self.series_dict['Title']))
 	g.LOG.info(backend.debug_message(636, g, result))
 	return result
