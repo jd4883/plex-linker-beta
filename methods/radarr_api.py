503cf8cbc369b207c0bf9470b8c83781485cd43b
root <editor>
2020-04-24 06:09:10 +0000

    logging cranked up to debug trying to figure out where the issues start; i think there is a parameter passing issue somewhere i havent caught

--- methods/radarr_api.py
+++ methods/radarr_api.py
@@ -15,65 +15,86 @@ class RadarrAPI(object):
 		self.api_key = re.sub('\n', '', pathlib.Path('/run/secrets/radarr_api_key').read_text())
 	
 	def refresh_movie(self, movie_id):
-		return self.request_post(f"{self.host_url}/command/RefreshMovie&seriesId={movie_id}")
+		update_movie = self.radarr_api_request(f"{self.host_url}/command/RefreshMovie&seriesId={movie_id}", "post")
+		return update_movie
 	
 	def rescan_movie(self, movie_id):
-		return self.request_post(f"{self.host_url}/command/RescanMovie&seriesId={movie_id}")
+		rescanned_movie = self.radarr_api_request(f"{self.host_url}/command/RescanMovie&seriesId={movie_id}", "post")
+		return rescanned_movie
 	
 	def movie_search(self, movie_id):
-		return self.request_post(f"{self.host_url}/command/MoviesSearch&seriesId={movie_id}")
+		movie_search = self.radarr_api_request(f"{self.host_url}/command/MoviesSearch&seriesId={movie_id}", "post")
+		return movie_search
 	
 	def get_movie_library(self):
-		return self.request_get(f"{self.host_url}/movie")
+		full_movie_library = self.radarr_api_request(f"{self.host_url}/movie")
+		return full_movie_library
 	
-	def lookup_movie(self, query, g):
-		query = self.request_get(f"{self.host_url}/movie/lookup?term={query}")
+	def lookup_movie(self, movie_lookup, g):
+		movie_lookup = self.radarr_api_request(f"{self.host_url}/movie/lookup?term={movie_lookup}")
 		for i in g.full_radarr_dict:
-			if 'tmdbId' in query and i['tmdbId'] == query[0]['tmdbId']:
-				query[0] = i
+			if 'tmdbId' in movie_lookup and i['tmdbId'] == movie_lookup[0]['tmdbId']:
+				movie_lookup[0] = i
 				break
-		return query
+		return movie_lookup
 	
-	def request_get(self, url, data = dict()):
-		backoff_timer = 30
-		get_request = dict()
-		for i in range(1 - 10):
-			try:
-				get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
-				break
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		return get_request
-	
-	def request_post(self, url, data = dict()):
-		backoff_timer = 30
-		post_request = dict()
-		for i in range(1 - 10):
-			try:
-				post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
-				break
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		return post_request
-	
-	def request_put(self, url, data = dict()):
-		backoff_timer = 30
-		put_request = dict()
-		for i in range(1 - 10):
-			try:
-				put_request = requests.put(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
-				break
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		return put_request
-	
-	def request_delete(self, url, data = dict()):
-		backoff_timer = 30
-		delete_request = dict()
-		for i in range(1 - 10):
-			try:
-				delete_request = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
-				break
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		return delete_request
+	def radarr_api_request(self, url, type = "get", data = dict()):
+		backoff_timer = 2
+		if type not in ["post", "put", "delete"]:
+			request_payload = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "put":
+			request_payload = requests.put(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "post":
+			request_payload = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "delete":
+			request_payload = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		time.sleep(backoff_timer)
+		return request_payload.json()
+
+# def request_get(self, url, data = dict()):
+# 	backoff_timer = 2
+# 	get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# for i in range(1 - 10):
+# 	# 	try:
+# 	# 		get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# 		break
+# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+# 	# 		time.sleep(backoff_timer)
+# 	time.sleep(backoff_timer)
+# 	return get_request
+#
+# def request_post(self, url, data = dict()):
+# 	backoff_timer = 2
+# 	post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	time.sleep(backoff_timer)
+# 	# for i in range(1 - 10):
+# 	# 	try:
+# 	# 		post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# 		break
+# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+# 	# 		time.sleep(backoff_timer)
+# 	return post_request
+#
+# def request_put(self, url, data = dict()):
+# 	backoff_timer = 2
+# 	put_request = requests.put(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# for i in range(1 - 10):
+# 	# 	try:
+# 	# 		put_request = requests.put(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# 		break
+# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+# 	# 		time.sleep(backoff_timer)
+# 	time.sleep(backoff_timer)
+# 	return put_request
+#
+# def request_delete(self, url, data = dict()):
+# 	backoff_timer = 2
+# 	delete_request = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	time.sleep(backoff_timer)
+# 	# for i in range(1 - 10):
+# 	# 	try:
+# 	# 		delete_request = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, json = data).json()
+# 	# 		break
+# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+# 	# 		time.sleep(backoff_timer)
+# 	return delete_request
--- methods/sonarr_api.py
+++ methods/sonarr_api.py
@@ -14,63 +14,80 @@ class SonarrAPI(object):
 		self.api_key = re.sub('\n', '', pathlib.Path('/run/secrets/sonarr_api_key').read_text())
 	
 	def get_episodes_by_series_id(self, series_id):
-		return self.request_get(f"{self.host_url}/episode?seriesId={series_id}")
+		episode_by_series_id = self.sonarr_api_request(f"{self.host_url}/episode?seriesId={series_id}")
+		return episode_by_series_id
 	
 	def get_episode_by_episode_id(self, episode_id):
-		return self.request_get(f"{self.host_url}/episode/{episode_id}")
+		episode_by_episode_id = self.sonarr_api_request(f"{self.host_url}/episode/{episode_id}")
+		return episode_by_episode_id
 	
 	def get_episode_file_by_episode_id(self, episode_id):
-		return self.request_get(f"{self.host_url}/episodefile/{episode_id}")
+		episode_file_by_id = self.sonarr_api_request(f"{self.host_url}/episodefile/{episode_id}")
+		return episode_file_by_id
 	
 	def get_root_folder(self):
-		try:
-			get_request = self.request_get(f"{self.host_url}/rootfolder")
-		except AttributeError:
-			get_request = dict()
+		get_request = self.sonarr_api_request(f"{self.host_url}/rootfolder")
 		return get_request
 	
 	def get_series(self):
-		series = dict()
-		try:
-			series = self.request_get(f"{self.host_url}/series")
-		except AttributeError:
-			pass
+		series = self.sonarr_api_request(f"{self.host_url}/series")
 		return series
 	
 	def refresh_series(self, series_id, data = dict()):
-		return self.request_post(f"{self.host_url}/command/RefreshSeries&seriesId={series_id}", data)
+		rescan_series = self.sonarr_api_request(f"{self.host_url}/command/RefreshSeries&seriesId={series_id}", data,
+		                                        "post")
+		return rescan_series
 	
 	def rescan_series(self, series_id, data = dict()):
-		return self.request_post(f"{self.host_url}/command/RescanSeries&seriesId={series_id}", data)
+		series_scan = self.sonarr_api_request(f"{self.host_url}/command/RescanSeries&seriesId={series_id}", data,
+		                                      "post")
+		return series_scan
 	
 	def lookup_series(self, series, g):
-		sonarr_series = self.request_get(f"{self.host_url}/series/lookup?term={series}")
+		sonarr_series = self.sonarr_api_request(f"{self.host_url}/series/lookup?term={series}")
 		if type(sonarr_series) == list:
 			sonarr_series = sonarr_series[0]
 		g.LOG.info(messaging.backend.debug_message(625, g, sonarr_series))
 		return sonarr_series
 	
-	def request_get(self, url, data = dict()):
-		backoff_timer = 30
-		get_request = dict()
-		for i in range(1 - 10):
-			try:
-				get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data)
-				get_request = get_request.json()
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		print(f"GET REQUEST: {get_request}")
-		return get_request
+	# def request_get(self, url, data = dict()):
+	# 	backoff_timer = 2
+	# 	get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+	# 	get_request = get_request.json()
+	# 	# for i in range(1 - 10):
+	# 	# 	try:
+	# 	# 		get_request = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+	# 	# 		get_request = get_request.json()
+	# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+	# 	# 		time.sleep(backoff_timer)
+	# 	time.sleep(backoff_timer)
+	# 	print(f"GET REQUEST: {get_request}")
+	# 	return get_request
+	#
+	# def request_post(self, url, data = dict()):
+	# 	backoff_timer = 2
+	# 	post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+	# 	post_request = post_request.json()
+	# 	# for i in range(1 - 10):
+	# 	# 	try:
+	# 	# 		post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+	# 	# 		post_request = post_request.json()
+	# 	# 		break
+	# 	# 	except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+	# 	# 		time.sleep(backoff_timer)
+	# 	time.sleep(backoff_timer)
+	# 	print(f"POST REQUEST: {post_request}")
+	# 	return post_request
 	
-	def request_post(self, url, data = dict()):
-		backoff_timer = 30
-		post_request = dict()
-		for i in range(1 - 10):
-			try:
-				post_request = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data)
-				post_request = post_request.json()
-				break
-			except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-				time.sleep(backoff_timer)
-		print(f"POST REQUEST: {post_request}")
-		return post_request
+	def sonarr_api_request(self, url, type = "get", data = dict()):
+		backoff_timer = 2
+		if type not in ["post", "put", "delete"]:
+			request_payload = requests.get(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "put":
+			request_payload = requests.put(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "post":
+			request_payload = requests.post(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		elif type == "delete":
+			request_payload = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, json = data)
+		time.sleep(backoff_timer)
+		return request_payload.json()
