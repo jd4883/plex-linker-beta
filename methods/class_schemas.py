2515ade7fa3be67e87e141b3f7bc07eb7d19edc5
root <editor>
2020-04-26 22:46:49 +0000

    ensuring dict to class object works reliably with preconfigured object; will be scaling this throughout the program and adding a way to deserialize a specific dataset to a dict if I keep this method rather than the way its currently handled

--- methods/class_schemas.py
+++ methods/class_schemas.py
@@ -4,16 +4,9 @@ from methods import Show
 
 
 class ShowLookupSchema(Schema):
-	added = fields.Str()
-	airTime = fields.Str(required = False)
 	certification = fields.Raw(required = False)
 	cleanTitle = fields.Raw(required = False)
-	firstAired = fields.DateTime(required = False)
-	genres = fields.List(fields.Str(required = False))
 	id = fields.Raw(required = True)
-	
-	images = fields.List(fields.Dict(required = False))
-	
 	imdbId = fields.Raw(required = True)
 	languageProfileId = fields.Raw(required = False)
 	lastInfoSync = fields.Raw(required = False)
@@ -24,21 +17,21 @@ class ShowLookupSchema(Schema):
 	profileId = fields.Raw(required = False)
 	qualityProfileId = fields.Raw(required = False)
 	ratings = fields.Raw(required = False)
-	
+	seasons = fields.Raw(required = False, default = list())
+	tags = fields.Raw(required = False, default = list())
+	title = fields.Raw(required = True, default = str())
+	added = fields.Str(required = False)
+	airTime = fields.Str(required = False)
+	firstAired = fields.DateTime(required = False)
+	genres = fields.List(fields.Str(required = False))
+	images = fields.List(fields.Dict(required = False))
 	remotePoster = fields.Url(required = False, default = str())
 	runtime = fields.Int(required = False, default = 20)
 	seasonCount = fields.Int(required = False, default = 1)
 	seasonFolder = fields.Bool(required = False, default = True)
-	
-	seasons = fields.Raw(required = False, default = list())
-	
 	seriesType = fields.Str(required = True, default = "anime")
 	sortTitle = fields.Str(required = False)
 	status = fields.Str(required = False, status = "continuing")
-	
-	tags = fields.Raw(required = False, default = list())
-	title = fields.Raw(required = True, default = str())
-	
 	titleSlug = fields.Str(required = False)
 	tvdbId = fields.Int(required = True)
 	tvMazeId = fields.Int(required = False)
@@ -46,6 +39,6 @@ class ShowLookupSchema(Schema):
 	useSceneNumbering = fields.Bool(required = False, default = False)
 	year = fields.Int(required = False)
 	
-	@post_load
+	@post_load()
 	def make_show(data, **kwargs):
 		return Show(**data)
