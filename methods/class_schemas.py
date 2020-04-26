4908fd5fca9423ecbf4539333417a587e4557b96
root <editor>
2020-04-26 22:21:10 +0000

    ensuring dict to class object works reliably with preconfigured object; will be scaling this throughout the program and adding a way to deserialize a specific dataset to a dict if I keep this method rather than the way its currently handled

--- methods/class_schemas.py
+++ methods/class_schemas.py
@@ -6,7 +6,7 @@ from methods import Show
 
 
 class ShowLookupSchema(Schema):
-	def __init__(self):
+	def __init__(self, show, many = False, partial = False):
 		self.added = fields.Str()
 		self.airTime = fields.Str(required = False)
 		self.certification = fields.Raw(required = False)
@@ -14,7 +14,9 @@ class ShowLookupSchema(Schema):
 		self.firstAired = fields.DateTime(required = False)
 		self.genres = fields.List(fields.Str(required = False))
 		self.id = fields.Raw(required = True)
+		
 		self.images = fields.List(fields.Dict(required = False))
+		
 		self.imdbId = fields.Raw(required = True)
 		self.languageProfileId = fields.Raw(required = False)
 		self.lastInfoSync = fields.Raw(required = False)
@@ -25,16 +27,21 @@ class ShowLookupSchema(Schema):
 		self.profileId = fields.Raw(required = False)
 		self.qualityProfileId = fields.Raw(required = False)
 		self.ratings = fields.Raw(required = False)
+		
 		self.remotePoster = fields.Url(required = False, default = str())
 		self.runtime = fields.Int(required = False, default = 20)
 		self.seasonCount = fields.Int(required = False, default = 1)
 		self.seasonFolder = fields.Bool(required = False, default = True)
+		
 		self.seasons = fields.Raw(required = False, default = list())
+		
 		self.seriesType = fields.Str(required = True, default = "anime")
 		self.sortTitle = fields.Str(required = False)
 		self.status = fields.Str(required = False, status = "continuing")
+		
 		self.tags = fields.Raw(required = False, default = list())
-		self.title = fields.Raw(required = True)
+		self.title = fields.Raw(required = True, default = str(show))
+		
 		self.titleSlug = fields.Str(required = False)
 		self.tvdbId = fields.Int(required = True)
 		self.tvMazeId = fields.Int(required = False)
