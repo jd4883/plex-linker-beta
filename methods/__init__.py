d5bf49a5a04b46c4bb647e69d5f1ec24c005ddb4
root <editor>
2020-04-26 00:34:08 +0000

    testing

--- methods/__init__.py
+++ methods/__init__.py
@@ -237,7 +237,8 @@ class Show(Movie, Globals):
 		import marshmallow
 		pprint(self.sonarr_series_dict)
 		schema = ShowParentLookupSchema()
-		marshmallow.pprint(f"SCHEMA DUMP TEST: {schema.dump(schema.load(self.sonarr_series_dict))}")
+		parsed_version = schema.load(self.sonarr_series_dict)
+		marshmallow.pprint(f"SCHEMA DUMP TEST: {parsed_version}")
 		breakpoint()
 		series_id = parse_item_out_of_series_dict('seriesId', self.sonarr_series_dict, self.inherited_series_dict)
 		self.series_id = self.inherited_series_dict.get("Series ID") if not series_id else series_id
