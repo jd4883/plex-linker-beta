63e8cbfd0164a3dbda7664e12c26940a89b4a96d
root <editor>
2020-04-26 08:07:56 +0000

    testing marshmallow, first api call parses out perfectly!

--- plex_linker/constructors/builds.py
+++ plex_linker/constructors/builds.py
@@ -23,7 +23,7 @@ def init_show_object(movie, series, g):
 def init_show(show, g):
 	print("TESTING HERE")
 	# lookup = iter(g.sonarr.lookup_series(show.show, g)).__next__()
-	lookup = g.sonarr.lookup_series(show.show, g)[0]
+	lookup = g.sonarr.lookup_series(show.title, g)[0]
 	schema = ShowLookupSchema()
 	schema.load(lookup)
 	schema.dump(show)
