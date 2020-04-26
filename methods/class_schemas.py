da2dbeff9becb3a05527fe53318801e23b30afb0
root <editor>
2020-04-26 22:36:56 +0000

    ensuring dict to class object works reliably with preconfigured object; will be scaling this throughout the program and adding a way to deserialize a specific dataset to a dict if I keep this method rather than the way its currently handled

--- methods/class_schemas.py
+++ methods/class_schemas.py
@@ -4,49 +4,48 @@ from methods import Show
 
 
 class ShowLookupSchema(Schema):
-	def __init__(self, show):
-		self.added = fields.Str()
-		self.airTime = fields.Str(required = False)
-		self.certification = fields.Raw(required = False)
-		self.cleanTitle = fields.Raw(required = False)
-		self.firstAired = fields.DateTime(required = False)
-		self.genres = fields.List(fields.Str(required = False))
-		self.id = fields.Raw(required = True)
-		
-		self.images = fields.List(fields.Dict(required = False))
-		
-		self.imdbId = fields.Raw(required = True)
-		self.languageProfileId = fields.Raw(required = False)
-		self.lastInfoSync = fields.Raw(required = False)
-		self.monitored = fields.Raw(required = False)
-		self.network = fields.Raw(required = False)
-		self.overview = fields.Raw(required = False)
-		self.path = fields.Raw(required = True)
-		self.profileId = fields.Raw(required = False)
-		self.qualityProfileId = fields.Raw(required = False)
-		self.ratings = fields.Raw(required = False)
-		
-		self.remotePoster = fields.Url(required = False, default = str())
-		self.runtime = fields.Int(required = False, default = 20)
-		self.seasonCount = fields.Int(required = False, default = 1)
-		self.seasonFolder = fields.Bool(required = False, default = True)
-		
-		self.seasons = fields.Raw(required = False, default = list())
-		
-		self.seriesType = fields.Str(required = True, default = "anime")
-		self.sortTitle = fields.Str(required = False)
-		self.status = fields.Str(required = False, status = "continuing")
-		
-		self.tags = fields.Raw(required = False, default = list())
-		self.title = fields.Raw(required = True, default = str(show))
-		
-		self.titleSlug = fields.Str(required = False)
-		self.tvdbId = fields.Int(required = True)
-		self.tvMazeId = fields.Int(required = False)
-		self.tvRageId = fields.Int(required = False)
-		self.useSceneNumbering = fields.Bool(required = False, default = False)
-		self.year = fields.Int(required = False)  # , default = int(datetime.datetime.year)
+	added = fields.Str()
+	airTime = fields.Str(required = False)
+	certification = fields.Raw(required = False)
+	cleanTitle = fields.Raw(required = False)
+	firstAired = fields.DateTime(required = False)
+	genres = fields.List(fields.Str(required = False))
+	id = fields.Raw(required = True)
+	
+	images = fields.List(fields.Dict(required = False))
+	
+	imdbId = fields.Raw(required = True)
+	languageProfileId = fields.Raw(required = False)
+	lastInfoSync = fields.Raw(required = False)
+	monitored = fields.Raw(required = False)
+	network = fields.Raw(required = False)
+	overview = fields.Raw(required = False)
+	path = fields.Raw(required = True)
+	profileId = fields.Raw(required = False)
+	qualityProfileId = fields.Raw(required = False)
+	ratings = fields.Raw(required = False)
+	
+	remotePoster = fields.Url(required = False, default = str())
+	runtime = fields.Int(required = False, default = 20)
+	seasonCount = fields.Int(required = False, default = 1)
+	seasonFolder = fields.Bool(required = False, default = True)
+	
+	seasons = fields.Raw(required = False, default = list())
+	
+	seriesType = fields.Str(required = True, default = "anime")
+	sortTitle = fields.Str(required = False)
+	status = fields.Str(required = False, status = "continuing")
+	
+	tags = fields.Raw(required = False, default = list())
+	title = fields.Raw(required = True, default = str(show))
+	
+	titleSlug = fields.Str(required = False)
+	tvdbId = fields.Int(required = True)
+	tvMazeId = fields.Int(required = False)
+	tvRageId = fields.Int(required = False)
+	useSceneNumbering = fields.Bool(required = False, default = False)
+	year = fields.Int(required = False)  # , default = int(datetime.datetime.year)
 	
 	@post_load
-	def make_show(self, data, **kwargs):
+	def make_show(data, **kwargs):
 		return Show(**data)
