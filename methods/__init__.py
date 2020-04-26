0daa5404ec337388f054b1503a90c5008540d8b5
root <editor>
2020-04-26 05:10:23 +0000

    testing marshmallow, first api call parses out perfectly!

--- methods/__init__.py
+++ methods/__init__.py
@@ -214,7 +214,7 @@ class Show(Movie, Globals):
 		self.year = datetime.datetime.year
 		self.episode = self.inherited_series_dict.get('Episode')
 		from pprint import pprint
-		pprint(ShowLookupSchema().load(self.sonarr_series_dict.__iter__().__next__()))
+		pprint(ShowLookupSchema().load(iter(self.sonarr_series_dict).__next__()))
 		self.anime_status = bool("anime" in self.seriesType)
 		self.padding = 3 if self.anime_status else int(os.environ['EPISODE_PADDING'])
 		parse_series.padded_episode_number(self, g)
