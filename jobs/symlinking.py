dbf4f58d74c1ff6f0f50c08b830814fcd1e2bf6f
root <editor>
2020-05-01 04:08:06 +0000

    Some cleanup done for radarr segment, a bit more work here and this should be a lot more efficient as less parsing is needed

--- jobs/symlinking.py
+++ jobs/symlinking.py
@@ -8,19 +8,21 @@ import messaging.frontend as message
 
 def symlink_force(show, g):
 	message.method_launch(g)
+	print(f"SYMLINK FORCE PRE PROCESS")
 	os.chdir(str(os.environ['HOST_MEDIA_PATH']))
 	process = subprocess.Popen(get_symlink_command_string(show), stderr = subprocess.DEVNULL,
 	                           stdout = subprocess.PIPE)
 	process = re.sub("'", "", str(process.communicate()[0])[3:-4])
 	
 	g.LOG.info(backend.debug_message(654, g, process))
+	print(f"HIT BREAKPOINT")
 	breakpoint()
 	message.method_exit(g)
 
 
 def get_symlink_command_string(show):
-	print(show.absolute_movie_file_path)
-	print(show.relative_show_file_path)
+	print(f"MOVIE PATH: {show.absolute_movie_file_path}")
+	print(f"SHOW PATH: {show.relative_show_file_path}")
 	return ["ln", "-fsvr", f"{show.absolute_movie_file_path}", f"{show.relative_show_file_path}"]
 
 
--- movies/movie/shows/shows_parse.py
+++ movies/movie/shows/shows_parse.py
@@ -1,6 +1,3 @@
-import json.decoder
-
-import messaging.backend as backend
 import messaging.frontend as message
 import methods
 from jobs.set_path_permissions import (set_permissions)
@@ -11,9 +8,6 @@ def parse_show_to_link(show, g):
 	message.method_launch(g)
 	map(symlink_force(show, g), show.shows_dictionary.items())
 	map(set_permissions(show, g), show.shows_dictionary.items())
-	# for _ in show.shows_dictionary.items():
-	# 	symlink_force(show, g), show.shows_dictionary.items()
-	# 	set_permissions(show, g)
 	message.method_exit(g)
 
 
@@ -24,20 +18,10 @@ def parse_shows_dictionary_object(movie, g):
 	for series in movie.shows_dictionary.keys():
 		if not isinstance(movie.shows_dictionary[series], dict):
 			continue
-		try:
-			show = methods.Show(g,
-			                    series,
-			                    movie.shows_dictionary[series],
-			                    movie.movie_dictionary)
-			g.sonarr.lookup_series(show)
-			show.initShow(g)
-		except json.decoder.JSONDecodeError:
-			continue
+		show = methods.Show(g,
+		                    series,
+		                    movie.shows_dictionary[series],
+		                    movie.movie_dictionary)
+		g.sonarr.lookup_series(show)
+		show.initShow(g)
 		parse_show_to_link(show, g)
-		try:
-			code = 640 if show.has_link or not show.episode else 641
-			g.LOG.info(backend.debug_message(code, g, movie.movieTitle, show.title))
-		except AttributeError:
-			code = 641
-		if code == 641:
-			parse_show_to_link(show, g)
