0c705f7467ccc1904e2df45cc41d7ee0e1b5a2ae
root <editor>
2020-05-01 07:07:33 +0000

    fixed double dot in titles

--- jobs/symlinking.py
+++ jobs/symlinking.py
@@ -9,7 +9,7 @@ import messaging.frontend as message
 def symlink_force(movie, show, g):
 	message.method_launch(g)
 	os.chdir(str(os.environ['HOST_MEDIA_PATH']))
-	process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path).replace('..', '.')}",
+	process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path)}",
 	                            f"{cleanString(show.relative_show_file_path)}"],
 	                           stderr = subprocess.DEVNULL,
 	                           stdout = subprocess.PIPE)
