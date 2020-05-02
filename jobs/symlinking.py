2f9b4ab42f092b93968fb16a7752904e21d0369a
root <editor>
2020-05-02 03:15:41 +0000

    attempting to improve logic for absolute movie path, it should be calculatable every time I think

--- jobs/symlinking.py
+++ jobs/symlinking.py
@@ -10,8 +10,8 @@ def symlink_force(movie, show, g):
 	message.method_launch(g)
 	if movie.hasFile:
 		os.chdir(str(os.environ['HOST_MEDIA_PATH']))
-		process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path, movie)}",
-		                            f"{cleanString(show.relative_show_file_path, movie)}"],
+		process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path)}",
+		                            f"{cleanString(show.relative_show_file_path)}"],
 		                           stderr = subprocess.DEVNULL,
 		                           stdout = subprocess.PIPE)
 		process = re.sub("'", "", str(iter(process.communicate()).__next__())[3:-4])
@@ -19,12 +19,12 @@ def symlink_force(movie, show, g):
 		g.LOG.info(backend.debug_message(654, g, process))
 		message.method_exit(g)
 
-def cleanString(string, movie):
-	strings_to_replace = {
+def cleanString(string):
+	find_and_replace = {
 			'..': '.',
 			':': '-',
 			}
-	for k, v in strings_to_replace.items():
+	for k, v in find_and_replace.items():
 		string = string.replace(k, v)
 	if string.startswith("/"):
 		string = string[1:]
