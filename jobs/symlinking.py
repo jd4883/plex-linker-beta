675a190a7592495da06c283a0f094c03161b1125
root <editor>
2020-05-02 03:08:44 +0000

    attempting to improve logic for absolute movie path, it should be calculatable every time I think

--- jobs/symlinking.py
+++ jobs/symlinking.py
@@ -8,15 +8,16 @@ import messaging.frontend as message
 
 def symlink_force(movie, show, g):
 	message.method_launch(g)
-	os.chdir(str(os.environ['HOST_MEDIA_PATH']))
-	process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path, movie)}",
-	                            f"{cleanString(show.relative_show_file_path, movie)}"],
-	                           stderr = subprocess.DEVNULL,
-	                           stdout = subprocess.PIPE)
-	process = re.sub("'", "", str(iter(process.communicate()).__next__())[3:-4])
-	
-	g.LOG.info(backend.debug_message(654, g, process))
-	message.method_exit(g)
+	if movie.hasFile:
+		os.chdir(str(os.environ['HOST_MEDIA_PATH']))
+		process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path, movie)}",
+		                            f"{cleanString(show.relative_show_file_path, movie)}"],
+		                           stderr = subprocess.DEVNULL,
+		                           stdout = subprocess.PIPE)
+		process = re.sub("'", "", str(iter(process.communicate()).__next__())[3:-4])
+		
+		g.LOG.info(backend.debug_message(654, g, process))
+		message.method_exit(g)
 
 def cleanString(string, movie):
 	# TODO: make a fancier method, this is really really basic
