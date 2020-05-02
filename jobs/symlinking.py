faaa89f6f459b742e603cf9b4865d01313ff4177
root <editor>
2020-05-02 02:30:50 +0000

    attempting to improve logic for absolute movie path, it should be calculatable every time I think

--- jobs/symlinking.py
+++ jobs/symlinking.py
@@ -9,8 +9,8 @@ import messaging.frontend as message
 def symlink_force(movie, show, g):
 	message.method_launch(g)
 	os.chdir(str(os.environ['HOST_MEDIA_PATH']))
-	process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path)}",
-	                            f"{cleanString(show.relative_show_file_path)}"],
+	process = subprocess.Popen(["ln", "-fsvr", f"{cleanString(movie.absolute_movie_file_path, movie)}",
+	                            f"{cleanString(show.relative_show_file_path, movie)}"],
 	                           stderr = subprocess.DEVNULL,
 	                           stdout = subprocess.PIPE)
 	process = re.sub("'", "", str(process.communicate()[0])[3:-4])
@@ -18,13 +18,12 @@ def symlink_force(movie, show, g):
 	g.LOG.info(backend.debug_message(654, g, process))
 	message.method_exit(g)
 
-def cleanString(string):
+def cleanString(string, movie):
 	# TODO: make a fancier method, this is really really basic
 	string = string.replace('..', '.')
 	string = string.replace(':', '-')
 	if string[:-4] != ".":
-		string = string.replace(".", " ")
-		string = string[:-4] + "." + string[-3:]
+		string = string[:-4] + "." + movie.extension.replace(".", "")
 	string = string.replace('mkv', '-')
 	if string.startswith("/"):
 		string = string[1:]
