967cfcbce12ea7babfb22fdd7a2beccabba7282e
root <editor>
2020-05-02 02:49:11 +0000

    attempting to improve logic for absolute movie path, it should be calculatable every time I think

--- methods/__init__.py
+++ methods/__init__.py
@@ -149,8 +149,14 @@ class Movie(Movies, Globals):
 						self.quality = \
 							self.movie_dictionary['Parsed Movie Quality'] = \
 							str(self.movieQuality['quality']['name'])
+						
 						baseQuality = re.sub(self.quality, str(), str(self.relativePath.split().pop()))
-						self.extension = re.sub("\s+REAL\.\W+$", "", baseQuality).replace(".","")
+						self.extension = re.sub("\s+REAL\.\W+$", "", baseQuality).replace(".", "")
+						print(self.quality)
+						print(baseQuality)
+						print(self.movieQuality)
+						print(self.extension)
+						breakpoint()
 						self.mediaInfo = items["movieFile"].pop("mediaInfo")  # placeholder may use this at
 						
 						self.sizeonDisk = items["movieFile"].pop("size")
