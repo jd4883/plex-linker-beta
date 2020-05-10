7a188ab4f228bacb6f76c7522f693a714f8983a5
root <editor>
2020-05-10 07:03:35 +0000

    minor fix for keyerror, will improve logging and file rotation later

--- IO/gets.py
+++ IO/gets.py
@@ -1,8 +1,12 @@
 import os
 
-
+# TODO: this segment needs a little TLC as it isn't great now but could be way better without a lot of work
 def get_collection_absolute_path_parsed_this_run():
-	return str(os.environ['YAML_FILE_CURRENT'])
+	try:
+		path = os.environ['YAML_FILE_CURRENT']
+	except KeyError:
+		path = get_collection_absolute_path_parsed_last_run()
+	return str(path)
 
 
 def get_collection_absolute_path_parsed_last_run():
