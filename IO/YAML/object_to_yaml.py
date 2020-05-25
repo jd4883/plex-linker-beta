65c672538830388b8643e52c1b72e81476283c21
root <editor>
2020-05-25 02:20:37 +0000

    adjusted file handling which is currently erroring

--- IO/YAML/object_to_yaml.py
+++ IO/YAML/object_to_yaml.py
@@ -15,17 +15,17 @@ from plex_linker.gets.path import get_media_collection_parsed_archives
 
 def write_python_dictionary_object_to_yaml_file(g):
 	method_launch(g)
-	Path(get_collection_absolute_path_parsed_this_run()).touch()
-	
-	yaml.dump(g.movies_dict, open(get_collection_absolute_path_parsed_this_run(), "w+"))
-	# need to review how this is set as there may be an error
-	
-	move(get_collection_absolute_path_parsed_last_run(), f"{get_media_collection_parsed_archives()}/collection_parsed_{time.strftime('%m-%d-%Y')}.yaml")
-	move(get_collection_absolute_path_parsed_this_run(), get_collection_absolute_path_parsed_last_run())
-	set_ownership_on_files_and_links(get_collection_absolute_path_parsed_last_run())
-	set_file_mask_with_chmod_on_files_and_links(get_collection_absolute_path_parsed_last_run(), g)
-	set_ownership_on_files_and_links(get_media_collection_parsed_archives())
-	set_file_mask_with_chmod_on_files_and_links(get_media_collection_parsed_archives(), g)
-	set_ownership_on_files_and_links(get_collection_absolute_path_parsed_this_run())
-	set_file_mask_with_chmod_on_files_and_links(get_collection_absolute_path_parsed_this_run(), g)
+	new = Path(get_collection_absolute_path_parsed_this_run())
+	new.touch()
+	yaml.dump(g.movies_dict, open(new, "w+"))
+	previous = get_collection_absolute_path_parsed_last_run()
+	archive_dir = get_media_collection_parsed_archives()
+	move(previous, f"{archive_dir}/collection_parsed_{time.strftime('%m-%d-%Y')}.yaml")
+	move(new, previous)
+	set_ownership_on_files_and_links(previous)
+	set_file_mask_with_chmod_on_files_and_links(previous, g)
+	set_ownership_on_files_and_links(archive_dir)
+	set_file_mask_with_chmod_on_files_and_links(archive_dir, g)
+	set_ownership_on_files_and_links(new)
+	set_file_mask_with_chmod_on_files_and_links(new, g)
 	method_exit(g)
